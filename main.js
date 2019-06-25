7ff466f5feda5192e1ff1b45d160ce71861ad22a
Apple <apple@Apples-MacBook-Pro-98.local>
2019-06-25 14:55:15 -0500

    To Do List

--- main.js
+++ main.js
@@ -1,20 +1,51 @@
-document.getElementById("button").innerHTML = "click here";
-var list = ["Luke Chiang", "Photoshoots", "$100+ perfume", "KWK", "Garlic Bread", "ALmond Milk", "SDPT on Facebook", "Goldfish", "Estonia", "Not Being Cold"];
-var button = document.getElementById("button");
-
-// function getItem () {
-//   alert("You have generated a random item!");
-//   document.write(list[Math.floor(Math.random() * 10)]);
-// }
-
-function randNum(){
-  var rand = Math.random();
-  var random = rand * list.length;
-  var randomNum = Math.floor(random);
-  return randomNum;
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
 
-button.addEventListener("click", function(){
-  document.querySelector("h1").innerHTML = (list[randNum()]);
-  document.querySelector("h1").style.padding = "50px 0px 0px 0px";
-});
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
